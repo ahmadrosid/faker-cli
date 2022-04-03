a5a4eba99040938c0b1090b664b6560ecc090c72
ahmadrosid <alahmadrosid@gmail.com>
2022-04-03 10:54:01 +0700

    feat: add sample custom attribute

--- debug.sh
+++ debug.sh
@@ -3,4 +3,4 @@ clear
 # cargo -q run -- -l 10 "{ username, email, password, first_name, last_name, zip_code, phone_number }"
 # cargo -q run "{ username, email, password, first_name, last_name, zip_code, phone_number }"
 # cargo -q run "{ full_name, text }"q.replace(")", "")
-cargo -q run "{ my_email(email) }"
+cargo -q run "{ my_email(email), nickname(username), kode_pos(zip_code), rahasia(password) }"
